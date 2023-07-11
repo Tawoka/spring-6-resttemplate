e9e2ecfbc925b0ced60c28089dc17bf7da84e44c
tawoka <github@spectral-darkness.org>
2023-07-11 09:16:09 +0200

    setting up first test to demonstrate the call is working

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,12 +1,27 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+  private final RestTemplateBuilder restTemplateBuilder;
 
   @Override
   public Page<BeerDTO> listBeers() {
+    RestTemplate template = restTemplateBuilder.build();
+
+    ResponseEntity<String> response = template.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+    System.out.println(response.getBody());
+
     return null;
   }
 
