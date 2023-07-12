af53e9dff610731f3d522fec9c097230a6ba3ab9
tawoka <github@spectral-darkness.org>
2023-07-12 09:13:34 +0200

    adding uri builder and query param

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -2,12 +2,14 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Service
@@ -18,10 +20,17 @@ public class BeerClientImpl implements BeerClient {
   private final RestTemplateBuilder restTemplateBuilder;
 
   @Override
-  public Page<BeerDTO> listBeers() {
+  public Page<BeerDTO> listBeers(@Nullable String name) {
     RestTemplate template = restTemplateBuilder.build();
 
-    ResponseEntity<BeerDTOPageImpl> response = template.getForEntity(GET_LIST_PATH, BeerDTOPageImpl.class);
+    UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_LIST_PATH);
+
+    if (name != null){
+      uriComponentsBuilder.queryParam("name", name);
+    }
+
+    ResponseEntity<BeerDTOPageImpl> response
+        = template.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
 
     System.out.println(response.getBody());
     System.out.println(response.getBody().getContent().get(0).getClass().getSimpleName());
