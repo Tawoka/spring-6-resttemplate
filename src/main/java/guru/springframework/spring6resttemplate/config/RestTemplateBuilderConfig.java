f65c3bf20bcd2fcfb2a2ffab265fdea24241616e
tawoka <github@spectral-darkness.org>
2023-07-14 12:15:50 +0200

    updating configuration

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -1,11 +1,10 @@
 package guru.springframework.spring6resttemplate.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.autoconfigure.web.client.*;
+import org.springframework.boot.web.client.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.util.*;
 
 @Configuration
 public class RestTemplateBuilderConfig {
@@ -20,17 +19,15 @@ public class RestTemplateBuilderConfig {
   String password;
 
   @Bean
-  RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
-
+  RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
     assert rootUrl != null;
 
-    RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
-    DefaultUriBuilderFactory defaultUriBuilderFactory =
-        new DefaultUriBuilderFactory(rootUrl);
+    RestTemplateBuilder builder = new RestTemplateBuilder();
+    DefaultUriBuilderFactory uriTemplateHandler = new DefaultUriBuilderFactory(rootUrl);
 
-    RestTemplateBuilder basicAuthenticationBuilder = builder.basicAuthentication(username, password);
-
-    return basicAuthenticationBuilder.uriTemplateHandler(defaultUriBuilderFactory);
+    return configurer.configure(builder)
+        .basicAuthentication(username, password)
+        .uriTemplateHandler(uriTemplateHandler);
   }
 
 }
